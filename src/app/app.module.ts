33e779349a7d1a6cbf547e80f1aab50b1c554245
juanmavelezpa <juanmavelezpa@gmail.com>
2022-09-13 16:18:53 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { routing, appRoutingProviders } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    AboutComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactusComponent,
+    ClientsComponent,
+    NotfoundComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, routing],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
